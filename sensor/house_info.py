45e7e6c4836abe4db9b888726432a448c2fccf25
Ryan Niklas <rniklas25@gmail.com>
2022-09-15 18:18:45 -0400

    Data files

--- sensor/house_info.py
+++ sensor/house_info.py
@@ -5,3 +5,7 @@ class HouseInfo:
     def __init__(self, data):
         self.data = data
 
+    def get_data_by_area(self, field, rec_area=0):
+        field_data = []
+        for record in self.data:
+            if rec_area == 0:
